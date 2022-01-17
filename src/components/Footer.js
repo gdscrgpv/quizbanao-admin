c18f10076214c6827c95b5f38767e6dbf5f2b465
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-17 22:43:14 +0530

    Created Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <>
+          {/* Footer */}
+          <footer>
+            <p>
+              © Bootadmin. All Rights Reserved. <br />
+              Designed and Developed by{" "}
+              <a href="https://sazzad.me">Sazzad Hossain</a>.
+            </p>
+          </footer>   
+        </>
+    )
+}
