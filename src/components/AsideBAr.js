dbf5461a0929c6c8a7d02e96dce92dae6998d05e
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-17 22:02:25 +0530

    Created Asidebar Component

---
+++ src/components/AsideBAr.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+export default function AsideBAr() {
+    return (
+        <>
+            <aside className="offcanvas-menu">
+          <div className="offcanvas-button">
+            <a href="#" className="btn wave-effect offcanvas-toggle font-20">
+              <span aria-hidden="true">×</span>
+            </a>
+          </div>
+          <h5 className="offcanvas-title">List Group</h5>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Morbi leo risus</li>
+            <li className="list-group-item">Porta ac consectetur ac</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <h5 className="offcanvas-title">List Group</h5>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Cras justo odio
+              <span className="badge badge-primary badge-pill">14</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Dapibus ac facilisis in
+              <span className="badge badge-primary badge-pill">2</span>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Morbi leo risus
+              <span className="badge badge-primary badge-pill">1</span>
+            </li>
+          </ul>
+          <h5 className="offcanvas-title">List Group Content</h5>
+          <div className="list-group list-group-flush">
+            <a
+              href="#"
+              className="list-group-item list-group-item-action flex-column align-items-start"
+            >
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+              </div>
+              <p className="mb-1">
+                Donec id elit non mi porta gravida at eget metus. Maecenas sed
+                diam eget risus varius blandit.
+              </p>
+              <small>Donec id elit non mi porta.</small>
+            </a>
+            <a
+              href="#"
+              className="list-group-item list-group-item-action flex-column align-items-start"
+            >
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">List group item heading</h5>
+                <small className="text-muted">3 days ago</small>
+              </div>
+              <p className="mb-1">
+                Donec id elit non mi porta gravida at eget metus. Maecenas sed
+                diam eget risus varius blandit.
+              </p>
+              <small className="text-muted">Donec id elit non mi porta.</small>
+            </a>
+            <a
+              href="#"
+              className="list-group-item list-group-item-action flex-column align-items-start"
+            >
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">List group item heading</h5>
+                <small className="text-muted">3 days ago</small>
+              </div>
+              <p className="mb-1">
+                Donec id elit non mi porta gravida at eget metus. Maecenas sed
+                diam eget risus varius blandit.
+              </p>
+              <small className="text-muted">Donec id elit non mi porta.</small>
+            </a>
+          </div>
+        </aside>  
+        </>
+    )
+}
