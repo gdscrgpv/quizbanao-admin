7627899058cd0e13815c8cd0ae7f7a5490623abd
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-19 20:38:55 +0530

    Added Dummy Questions

---
+++ src/questions.js
@@ -0,0 +1,196 @@
+
+// Who developed Yahoo?
+// (A) Dennis Ritchie & Ken Thompson
+// (B) David Filo & Jerry Yang
+// (C) Vint Cerf & Robert Kahn
+// (D) Steve Case & Jeff Bezos
+
+// B
+
+// 'DB' computer abbreviation usually means?
+// (A) Database
+// (B) Double Byte
+// (C) Data Block
+// (D) Driver Boot
+
+// B
+
+// '.INI' extension refers usually to what kind of file?
+// (A) Image file
+// (B) System file
+// (C) Hypertext related file
+// (D) Image Color Matching Profile file
+
+// B
+
+//  The sampling rate, (how many samples per second are stored) for a CD is?
+// (A) 48.4 kHz
+// (B) 22,050 Hz
+// (C) 44.1 kHz
+// (D) 48 kHz
+
+// C
+
+// What is FMD?
+// (A) Fast-Ethernet Measuring Device
+// (B) Flashing Media Diode
+// (C) Fluorescent Multi-Layer Disc
+// (D) Flash Media Driver
+
+// C
+
+// What does SSL stand for?
+// (A) Secure Socket Layer
+// (B) System Socket Layer
+// (C) Superuser System Login
+// (D) Secure System Login
+
+// A    
+
+
+// The Central Processing Unit is an embedded chip that acts as the 'brains' of a computer. What Intel chip was used in the Altair (the first real personal computer)?
+// (A) 6502
+// (B) 8080
+// (C) 6400
+// (D) 8286
+
+// B
+
+// In what year was the "@" chosen for its use in e-mail addresses?
+// (A) 1976
+// (B) 1972
+// (C) 1980
+// (D) 1984
+
+// B
+
+
+
+
+export default questions = {
+    "question": {
+        "text": "Ministry of Earth Science is set up in …",
+        "options": {
+            "option1": "1992",
+            "option2": "1999",
+            "option3": "2002",
+            "option4": "2006"
+        },
+        "time": 10,
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "answer": "option4"
+    },
+    "question1": {
+        "imageUrl": "",
+        "text": "Which among the following missiles is popular as “Fire-and-Forget?”",
+        "options": {
+            "option1": "Agni",
+            "option2": "Nag",
+            "option3": "Prithvi",
+            "option4": "Trishul"
+        },
+        "time": 10,
+        "answer": "option2"
+    },
+    "question2": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-16317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "options": {
+            "option1": "New Delhi",
+            "option2": "Pune",
+            "option3": "Hyderabad",
+            "option4": "Manesar"
+        },
+        "text": "The Center for DNA Fingerprinting and Diagnostics is located in …",
+        "time": 10,
+        "answer": "option3"
+    },
+    "question3": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-16317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "options": {
+            "option1": "Agni",
+            "option2": "Nag",
+            "option3": "Prithvi",
+            "option4": "Trishul"
+        },
+        "text": "Which among the following missiles is popular as “Fire-and-Forget?”",
+        "time": 10,
+        "answer": "option2"
+    },
+    "question4": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-16317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "options": {
+            "option1": "CISC",
+            "option2": "RISC",
+            "option3": "EPIC",
+            "option4": "All of the Mentioned"
+        },
+        "text": "Which among the following is a type of MicroProcessor?",
+        "time": 10,
+        "answer": "option4"
+    },
+    "question5": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-165317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "text": "MPG's extension refer usually to what kind of file",
+        "options": {
+            "option1": "Word Perfect Document file",
+            "option2": "MS Office document",
+            "option3": "Animation/movie file",
+            "option4": "Image file"
+        },
+        "time": 10,
+        "answer": "option3"
+    },
+    "question6": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "text": "What is part of a database that holds only one type of information?",
+        "options": {
+            "option1": "Report",
+            "option2": "Field",
+            "option3": "Record",
+            "option4": "File"
+        },
+        "time": 10,
+        "answer": "option2"
+    },
+    "question7": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "text": "Who developed Yahoo?",
+        "options": {
+            "option1": "Dennis Ritchie & Ken Thompson",
+            "option2": "David Filo & Jerry Yang",
+            "option3": "Vint Cerf & Robert Kahn",
+            "option4": "Steve Case & Jeff Bezos"
+        },
+        "time": 10,
+        "answer": "option2"
+    },
+    "question8": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "text": "'DB' Computer abbreviation usually means?",
+        "options": {
+            "option1": "Data Base",
+            "option2": "Double Byte",
+            "option3": "Data Block",
+            "option4": "Driver Boot"
+        },
+        "time": 10,
+        "answer": "option1"
+    },
+    "question9": {
+        "imageUrl": "https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "text": "'.INI' extension usually means?",
+        "options": {
+            "option1": "Image File",
+            "option2": "System File",
+            "option3": "Hypertext Related File",
+            "option4": " Image Color Matching Profile file"
+        },
+        "time": 10,
+        "answer": "option2"
+    },
+    
+
+
+
+
+}
