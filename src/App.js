512ef1c52b2aad06819ae2938eef49e25af92224
sonali-t <sonalist99@gmail.com>
2022-01-25 20:06:00 +0530

    ui for view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import firebase from "./firebase";
@@ -13,27 +13,54 @@ import QuizzesTable from "./components/QuizzesTable";
 import Footer from "./components/Footer";
 import questions from "./questions";
 import DeleteQuiz from "./components/DeleteQuiz";
+import ViewQueAns from "./components/ViewQueAns";
+import MainDashboard from "./components/MainDashboard";
 
 function App() {
   // Get collection named quizzes from Firebase Firestore
- const [quizzes, setQuizzes] = useState([]);
+  const [deleteID, setDeleteID] = useState(10);
+  const [quizzes, setQuizzes] = useState([]);
   async function getQuizzes() {
     const quizzesSnapshot = await getDocs(collection(firebase, "quizzes"));
-    const quizzesList = quizzesSnapshot.docs.map(doc => {return {id: doc.id,...doc.data()}});
+    const quizzesList = quizzesSnapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
     setQuizzes(quizzesList);
     // To set Data in Doc
-    // await setDoc(doc(firebase, "quizzes", "test222"),{active:true,questions: questions});
+    // await setDoc(doc(firebase, "quizzes", "test2212"),{active:true,questions: questions});
   }
   useEffect(() => {
-  getQuizzes();
+    getQuizzes();
   }, []);
+  // useEffect(() => {
 
+  // }, [setDeleteID,deleteID]);
+  const changeID=(val) =>{
+    setDeleteID(val)
+  }
+  console.log("app",deleteID)
   return (
+    
     <>
       <div id="dashboardPage">
         <Topbar />
         <Sidebar />
         <main>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <MainDashboard
+                  quizzes={quizzes}
+                  idQuiz={deleteID}
+                  setIdQuiz={setDeleteID}
+                  changeID={changeID}
+                />
+              }
+            />
+
+            {/* <>
           <BreadCrubmb />
 
           <div className="container-fluid">
@@ -41,6 +68,19 @@ function App() {
             <QuizzesTable quizzes={quizzes}/>
             
           </div>
+          </> */}
+            {/* <Route
+              exact
+              path="/view/:id"
+              render={(props)=>{<ViewQueAns {...props} />}}
+            /> */}
+            <Route
+              exact
+              path="/view/:id"
+              element={<ViewQueAns   />}
+            />
+          </Routes>
+
           <Footer />
         </main>
       </div>
--- src/components/QuizzesTable.js
+++ src/components/QuizzesTable.js
@@ -1,114 +1,115 @@
-import React,{useState,useEffect} from 'react'
-import DeleteQuiz from './DeleteQuiz';
+import React, { useState, useEffect } from "react";
+import DeleteQuiz from "./DeleteQuiz";
 import firebase from "../firebase";
 import { collection, doc, deleteDoc } from "firebase/firestore";
-
-
-
+import ViewQueAns from "./ViewQueAns";
 
 export default function QuizzesTable(props) {
   // const delState = 0;
-  const deleteQuiz = async(id)=>{
-    const quizDel = doc(firebase,"quizzes", id); 
+  const deleteQuiz = async (id) => {
+    const quizDel = doc(firebase, "quizzes", id);
     await deleteDoc(quizDel);
     // delState=!delState;
     window.location.reload();
-    }
+  };
+  
   // useEffect(() => {
-    
-  // }, [delState]);
-  
-  
+  //   const setid =(id) => {
+                                
+  //     props.setIdQuiz(id);
+  //     console.log("vie",id);
+  //   }
+
+  // }, [props]);
+
   console.log(props.quizzes);
-  const [deleteID, setDeleteID] = useState();
-    return (
-        <>
-          <div className="row">
-              <div className="col-sm-12">
-                <div className="card">
-                  <div className="card-body">
-                    {/* title */}
-                    <div className="d-md-flex align-items-center">
-                      <div>
-                        <h3 className="card-title">Quizzes</h3>
-                      </div>
-                      <div className="ml-auto">
-                        <div className="dl">
-                          <select className="custom-select">
-                            <option value={0} selected>
-                              Monthly
-                            </option>
-                            <option value={1}>Daily</option>
-                            <option value={2}>Weekly</option>
-                            <option value={3}>Yearly</option>
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                    {/* title */}
+  
+  return (
+    <>
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="card">
+            <div className="card-body">
+              {/* title */}
+              <div className="d-md-flex align-items-center">
+                <div>
+                  <h3 className="card-title">Quizzes</h3>
+                </div>
+                <div className="ml-auto">
+                  <div className="dl">
+                    <select className="custom-select">
+                      <option value={0} selected>
+                        Monthly
+                      </option>
+                      <option value={1}>Daily</option>
+                      <option value={2}>Weekly</option>
+                      <option value={3}>Yearly</option>
+                    </select>
                   </div>
-                  <div className="table-responsive">
-                    <table className="table v-middle">
-                      <thead>
-                        <tr className="bg-light">
-                          <th className="border-top-0">Sno.</th>
-                          <th className="border-top-0">ID</th>
-                          <th className="border-top-0">Active</th>
-                          <th className="border-top-0">Actions</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-
-                       
-
-                      {
-                        props.quizzes.map((quiz,index)=>{
-                          return (
-                            <>
-                             <tr>
-                          <td>
-                            
-                            {index+1}
-                          </td>
+                </div>
+              </div>
+              {/* title */}
+            </div>
+            <div className="table-responsive">
+              <table className="table v-middle">
+                <thead>
+                  <tr className="bg-light">
+                    <th className="border-top-0">Sno.</th>
+                    <th className="border-top-0">ID</th>
+                    <th className="border-top-0">Active</th>
+                    <th className="border-top-0">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {props.quizzes.map((quiz, index) => {
+                    return (
+                      <>
+                        <tr>
+                          <td>{index + 1}</td>
                           <td>{quiz.id}</td>
                           <td>{quiz.active ? "Active" : "InActive"}</td>
                           <td>
-                            <a href="#" className="btn btn-outline-primary">
+                            <a
+                              href={"/view/"+quiz.id}
+                              className="btn btn-outline-primary"
+                              onClick={()=>{props.changeID(quiz.id);
+                              console.log("view",props.idQuiz)}}
+                            >
+                              
                               <i className="mdi mdi-eye" />
                             </a>
                             <a href="#" className="btn btn-outline-success">
                               <i className="mdi mdi-pencil" />
                             </a>
-                            
-                        
-                            
-                           
-                            <a href="#" role="button" type="button" data-toggle="modal" data-target="#exampleModalCenter" className="btn btn-outline-danger" 
-                            onClick={()=>{setDeleteID(quiz.id)}}>
-                            
+
+                            <a
+                              href="#"
+                              role="button"
+                              type="button"
+                              data-toggle="modal"
+                              data-target="#exampleModalCenter"
+                              className="btn btn-outline-danger"
+                              onClick={() => {
+                                props.setIdQuiz(quiz.id);
+                              }}
+                            >
                               <i className="mdi mdi-trash-can" />
                             </a>
-                            
                           </td>
                         </tr>
-                            </>
-                          )
-                        }
-                        )
-                      }
-                        
-                      
-
-                      </tbody>
-                    </table>
-                    
-                  </div>
-                  <DeleteQuiz d={deleteID} 
-                  delFunc={deleteQuiz}
-                               />
-                </div>
-              </div>
-            </div>  
-        </>
-    )
+                      </>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <DeleteQuiz d={props.idQuiz} delFunc={deleteQuiz} />
+            
+          </div>
+        </div>
+      </div>
+      {/* <ViewQueAns ID = {deleteID} /> */}
+    </>
+    
+  );
 }
