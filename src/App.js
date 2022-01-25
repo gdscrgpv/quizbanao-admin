6278ef86811da82f8cfae9e1e8a9f726acc648be
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-25 23:04:03 +0530

    Fetched Responses in Responses Page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
   // Get collection named quizzes from Firebase Firestore
   const [deleteID, setDeleteID] = useState(10);
   const [quizzes, setQuizzes] = useState([]);
+  const [responses, setResponses] = useState([]);
+
   async function getQuizzes() {
     const quizzesSnapshot = await getDocs(collection(firebase, "quizzes"));
     const quizzesList = quizzesSnapshot.docs.map((doc) => {
@@ -21,8 +23,17 @@ function App() {
     // To set Data in Doc
     // await setDoc(doc(firebase, "quizzes", "test2212"),{active:true,questions: questions});
   }
+  async function getResponses() {
+    const responsesSnapshot = await getDocs(collection(firebase, "users"));
+    const responsesList = responsesSnapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
+
+    setResponses(responsesList);
+  }
   useEffect(() => {
     getQuizzes();
+    getResponses();
   }, []);
   const changeID = (val) => {
     setDeleteID(val);
@@ -48,7 +59,7 @@ function App() {
           path="/responses"
           element={
             <Responses
-              quizzes={quizzes}
+              quizzes={responses}
               idQuiz={deleteID}
               setIdQuiz={setDeleteID}
               changeID={changeID}
--- src/pages/Responses.js
+++ src/pages/Responses.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import MainLayout from '../layouts/MainLayout';
-import StatCards from '../components/StatCards';
 import QuizzesTable from '../components/QuizzesTable';
 
 export default function Responses({quizzes,idQuiz,setIdQuiz,changeID}) {
   return (
   <MainLayout>
     <div className="container-fluid">
-      <StatCards quizzes={quizzes} />
       <QuizzesTable quizzes={quizzes} idQuiz={idQuiz} setIdQuiz={setIdQuiz} changeID={changeID} />
     </div>
   </MainLayout>
