a295dc05c51afb39f71815b6189e683ab010734c
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-03 00:23:56 +0530

    Created UI for View Quiz Page

--- src/pages/ViewQuiz.js
+++ src/pages/ViewQuiz.js
@@ -14,6 +14,7 @@ export default function ViewQuiz() {
     setActive(data.active);
     console.log(data);
   }, []);
+
   return (
     <MainLayout pageName={"View Quiz " + id}>
       <div className="container">
@@ -24,10 +25,44 @@ export default function ViewQuiz() {
         )}
 
         <hr />
-        <div>
-          {Object.keys(Questions).map((key,question) => (
-            <div key={key}>
-              <h3>{key}</h3>
+        <div style={{
+          maxWidth: "800px",
+          margin: "0 auto",
+        }}>
+          {Object.keys(Questions).map((key,index) => (
+            <div style={{
+              padding: "5%",
+              boxShadow: "0px 0px 10px #ccc",
+              borderRadius: "10px",
+              margin: "10px",
+              backgroundColor: "#fff"
+            }} key={key}>
+              <span style={{
+                fontSize: "1.3em",
+                fontWeight: "bold",
+                color: "#444"
+              }}>{index+1+") "+Questions[key].text}</span>
+              <hr />
+              <div className="row">
+                <div className="col-md-6">
+                  {Object.keys(Questions[key].options).map((optionKey,optionIndex) => (
+                    <div key={"options"+optionKey}
+                    style={{
+                      padding: "5%",
+                      borderRadius: "5px",
+                      marginTop: "12px",
+                      backgroundColor: Questions[key].answer === optionKey ? "rgb(131, 234, 155)" : "#ddd"
+                    }}>
+                      {Questions[key].options[optionKey]}
+                      </div>
+                  ))}
+                  </div>
+                <div className="col-md-6">
+              <img src={Questions[key].imageUrl} alt="" style={{
+                maxHeight: "300px",
+              }} />
+              </div>
+              </div>
               </div>
           ))}
         </div>
