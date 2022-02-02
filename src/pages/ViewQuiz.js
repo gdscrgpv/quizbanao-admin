d975bf1a6b768199698ebd84dcbc81d5ee87f897
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-02 23:55:14 +0530

    Fetched Data in View Quiz Page

--- src/pages/ViewQuiz.js
+++ src/pages/ViewQuiz.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout"; 
+import { getDocument } from "../helpers/firebase";
+import MainLayout from "../layouts/MainLayout";
 
 export default function ViewQuiz() {
-    
+  const [Questions, setQuestions] = useState({});
+  const [active, setActive] = useState(false);
   const params = useParams();
   const id = params.id;
+  useEffect(async () => {
+    const data = await getDocument("quizzes", id);
+    setQuestions(data.questions);
+    setActive(data.active);
+    console.log(data);
+  }, []);
   return (
-    <MainLayout pageName={"View Quiz "+id}>
-      <div className="container-fluid">
-          Hello World
+    <MainLayout pageName={"View Quiz " + id}>
+      <div className="container">
+        {active ? (
+          <h2 className="text-success">Active</h2>
+        ) : (
+          <h2 className="text-danger"></h2>
+        )}
+
+        <hr />
+        <div>
+          {Object.keys(Questions).map((key,question) => (
+            <div key={key}>
+              <h3>{key}</h3>
+              </div>
+          ))}
+        </div>
       </div>
     </MainLayout>
-    );
+  );
 }
