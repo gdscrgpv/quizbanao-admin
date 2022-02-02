bf73eb187876eac810bedbc2f8a5340ae0b6c0bf
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-02 21:22:16 +0530

    Added Updation Feature in Responses

--- src/components/ResponsesEditModal.js
+++ src/components/ResponsesEditModal.js
@@ -1,18 +1,40 @@
 import React, { useEffect, useState } from "react";
-import {getDocument} from '../helpers/firebase';
+import { updateDocument, getDocument } from "../helpers/firebase";
 
 function ResponsesEditModal(props) {
-  const [responseData, setResponseData] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [marks, setMarks] = useState("");
+  const [quizId, setQuizId] = useState("");
+  const [time_taken, setTime_taken] = useState("");
   useEffect(async () => {
     console.log(`Looking for ${props.id}`);
-    const data = await getDocument('users', props.id);
-    console.log("Response Data: " ,data);
-    setResponseData(data);
+    const data = await getDocument("users", props.id);
+    setFullName(data.fullName);
+    setEmail(data.email);
+    setMarks(data.marks);
+    setQuizId(data.quizId);
+    setTime_taken(data.time_taken);
   }, [props.id]);
+
+  async function updateData() {
+    const formdata = {
+      fullName,
+      email,
+      marks,
+      quizId,
+      time_taken,
+    };
+    console.log("Updating Data......");
+    await updateDocument("users", props.id, formdata);
+    console.log("Data Updated......");
+  }
+    
+  
 
   return (
     <>
-       <div
+      <div
         className="modal fade"
         id="responsesEditModal"
         tabindex="-1"
@@ -35,33 +57,66 @@ function ResponsesEditModal(props) {
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div className="modal-body">
+            <div className="modal-body">              
+              <form>
+                <div className="form-group">
+                  <label htmlFor="fullName">Full Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="fullName"
+                    placeholder="Full Name"
+                    value={fullName}
+                    onChange={(e) => {setFullName(e.target.value)}}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value)}}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="marks">Marks</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="marks"
+                    placeholder="Marks"
+                    value={marks}
+                    onChange={(e) => {setMarks(e.target.value)}}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="quizId">Quiz Id</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="quizId"
+                    placeholder="Quiz Id"
+                    value={quizId}
+                    onChange={(e) => {setQuizId(e.target.value)}}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="time_taken">Time Taken</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="time_taken"
+                    placeholder="Time Taken"
+                    value={time_taken}
+                    onChange={(e) => {setTime_taken(e.target.value)}}
+                  />
+                </div>
+              </form>
               
-              {responseData &&
-                  <table className="table table-borderless">
-                    <tr>
-                      <th>Name</th>
-                      <td>{responseData.fullName}</td>
-                    </tr>
-                    <tr>
-                      <th>Email</th>
-                      <td>{responseData.email}</td>
-                    </tr>
-                    <tr>
-                      <th>Marks</th>
-                      <td>{responseData.marks}</td>
-                    </tr>
-                    <tr>
-                      <th>Quiz Id</th>
-                      <td>{responseData.quizId}</td>
-                    </tr>
-                    <tr>
-                      <th>Time Taken</th>
-                      <td>{responseData.time_taken}</td>
-                    </tr>
-                  </table>
-  }
-               </div>
+            </div>
             <div className="modal-footer">
               <button
                 type="button"
@@ -69,6 +124,13 @@ function ResponsesEditModal(props) {
                 data-dismiss="modal"
               >
                 Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => updateData()}
+              >
+                Update
               </button>
             </div>
           </div>
