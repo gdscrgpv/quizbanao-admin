729869a60ec5458bf5b1f3e6f741cfb18301841e
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-02 21:44:19 +0530

    Improved Breadcrumb Component

--- src/components/BreadCrubmb.js
+++ src/components/BreadCrubmb.js
@@ -1,28 +1,22 @@
 import React from 'react'
 
-export default function BreadCrubmb() {
+export default function BreadCrubmb(props) {
     return (
         <>
           <div className="page-breadcrumb">
             <div className="row">
               <div className="col-6">
-                <h4 className="page-title">Dashboard</h4>
+                <h4 className="page-title">{props.pageName ?? 'Dashboard'}</h4>
                 <nav aria-label="breadcrumb">
                   <ol className="breadcrumb">
                     <li className="breadcrumb-item">
                       <a href="/">Home</a>
                     </li>
                     <li className="breadcrumb-item active" aria-current="page">
-                      Dashboard
+                      {props.pageName ?? 'Dashboard'}
                     </li>
                   </ol>
                 </nav>
-              </div>
-              <div className="col-6">
-                <div className="text-right">
-                  <small>Users</small>
-                  <h5 className="text-info">3,458</h5>
-                </div>
               </div>
             </div>
           </div>  
